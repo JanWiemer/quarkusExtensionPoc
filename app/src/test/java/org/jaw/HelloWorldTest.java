53679c1b2508b829fc1f6f35120e67f95b6e667d
Jan Wiemer <jan.wiemer@ssi-schaefer.com>
2026-04-02 12:48:18 +0200

    Finished logging POC

--- app/src/test/java/org/jaw/HelloWorldTest.java
+++ app/src/test/java/org/jaw/HelloWorldTest.java
@@ -3,12 +3,23 @@ package org.jaw;
 import io.quarkus.logging.Log;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
 
 @QuarkusTest
 public class HelloWorldTest {
 
+  private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
   @Test
   public void helloWorldTest() {
-    Log.info("Hello Test World!");
+    Log.info("INFO Hello Test World!");
+    log.info("INFO Hello World via SLF4J!");
+    Log.warn("WARN Hello Test World!");
+    log.warn("WARN Hello World via SLF4J!");
+    Log.error("ERROR Hello Test World!");
+    log.error("ERROR Hello World via SLF4J!");
   }
 }
